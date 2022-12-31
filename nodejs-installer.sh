1137b7cc26fb8f7848c1b6154866ac0d827358c0
uint64-dev <121733684+uint64-dev@users.noreply.github.com>
2022-12-31 23:24:11 +0100

    Release 1.0

---
+++ nodejs-installer.sh
@@ -0,0 +1,4 @@
+clear
+echo "This installer comes with both Node.JS and NPM (Node Package Manager). The installation should take no more than 10 minutes. Click any key to proceed with the instllation."
+read input
+sudo apt install nodejs npm
